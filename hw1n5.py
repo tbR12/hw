ee077e2705a6f1eb095167e5ac7bd3abc267f1bb
tbR12 <56205442+tbR12@users.noreply.github.com>
2019-10-11 22:29:51 +0300

    Add files via upload

---
+++ hw1n5.py
@@ -0,0 +1,14 @@
+a = float( input())
+b = float( input())
+c = float( input())
+D = b**2 - 4*a*c
+if D > 0:
+    x1 = (-b + D**0.5 )/(2*a)
+    x2 = (-b - D**0.5)/(2*a)
+    print("X1:",x1,"X2:", x2)
+elif D == 0:
+    x1=(-b)/(2*a)
+    print("X1:", x1)
+elif D < 0:
+    print("") 
+    
