5980b0854e615074be88e5ea58fb47d24a11e159
tbR12 <56205442+tbR12@users.noreply.github.com>
2019-10-12 10:11:37 +0300

    Update hw1n5.py

--- hw1n5.py
+++ hw1n5.py
@@ -2,13 +2,15 @@ a = float( input())
 b = float( input())
 c = float( input())
 D = b**2 - 4*a*c
-if D > 0:
+if (D > 0 and a > 0):
     x1 = (-b + D**0.5 )/(2*a)
     x2 = (-b - D**0.5)/(2*a)
     print("X1:",x1,"X2:", x2)
-elif D == 0:
+elif (D == 0 and a > 0):
     x1=(-b)/(2*a)
     print("X1:", x1)
-elif D < 0:
-    print("") 
+elif (D < 0 and a >0):
+    print("")
+else:
+    print('')  
     
